40411f9baff4d79f05fc528f16f75dcd20d4907b
jef4490 <jeffrey.hechler@gmail.com>
2017-03-18 15:00:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const app = "I don't do much."
+
+function bumpCounter() {
+  var counter = 0
+
+  function addBump() {
+    counter ++;
+  }
+  function getBumps() {
+    return counter;
+  }
+
+  return { addBump, getBumps };
+}
+
+// var { addBump, getBump } = bumpCounter();
+
+function createAnimal(animalType) {
+  function weapon(deadlyDevice) {
+    return { animalType: [animalType], deadlyDevice: [deadlyDevice]};
+  }
+
+  return weapon;
+}
+
+var sharkCreator = createAnimal("Shark")
+
+var sharkWithFrickinLaserbeam = sharkCreator("Laserbeam")
+var sharkWithFrickinCannon = sharkCreator("Cannon")
